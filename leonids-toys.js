1e88e30619155d579ba3ff3bffaaeb1c4e711656
Dakota <dakota.c.upchurch@gmail.com>
2022-02-03 21:08:16 -0600

    converted toy objects to an array of toys

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,15 +1,43 @@
-const ball = {
-    id = 1,
-    cost = 5,
-    weight = 1.5,
-    shape = "round"
-};
+// const ball = {
+//     id = 1,
+//     cost = 5,
+//     weight = 1.5,
+//     shape = "round"
+// };
 
-const train = {
-    id = 2,
-    cost = 10,
-    weight = 3.2,
-    shape = "rectangle"
-};
+// const train = {
+//     id = 2,
+//     cost = 10,
+//     weight = 3.2,
+//     shape = "rectangle"
+// };
 
-const 
+// const puzzle = {
+//     id = 3,
+//     cost = 7,
+//     weight = 2.5,
+//     shape = "square"
+// };
+
+const toys = [
+    {
+        id: 1,
+        cost: 5,
+        weight: 1.5,
+        shape: "round"
+    },
+    {
+        id: 2,
+        cost: 10,
+        weight: 3.2,
+        shape: "rectangle"
+    },
+    {
+        id: 3,
+        cost: 7,
+        weight: 2.5,
+        shape: "square"
+    }
+]
+
+console.log(toys)
