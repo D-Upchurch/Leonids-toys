4d459a34a0e96499d270604fe1e2b39b1d9785a1
Dakota <dakota.c.upchurch@gmail.com>
2022-02-03 20:46:02 -0600

    first commit in way too long, get excited

---
+++ leonids-toys.js
@@ -0,0 +1,15 @@
+const ball = {
+    id = 1,
+    cost = 5,
+    weight = 1.5,
+    shape = "round"
+};
+
+const train = {
+    id = 2,
+    cost = 10,
+    weight = 3.2,
+    shape = "rectangle"
+};
+
+const 
